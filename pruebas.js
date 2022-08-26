25ced56ecf687b4e64c0c21febdd6e919c2e71ad
MarceloYudis <marceloyudisortiz@gmail.com>
2022-08-25 23:20:55 -0400

    concatenaciones y parametros por defecto para funciones

---
+++ pruebas.js
@@ -0,0 +1,29 @@
+//ECMA SCRIPT 6
+//parametros por defecto
+function pruebaDeParametros(name = 'oscar', edad = 32, pais = "CO") {
+    console.log(name, edad, pais);
+}
+
+pruebaDeParametros();
+
+// metodo antecesor
+function metodoAntecesor(nombre, edad, pais) {
+    var nombre = nombre || 'pedro';
+    var edad = edad || '22';
+    var pais = pais || 'ARG';
+
+    console.log(nombre, edad, pais);
+}
+
+//////////////////////////////////////
+//Concatenacion ECMA6
+let saludo = "Buenas tardes";
+let pregunta = "¿Que hora tienes?";
+let fraseCompleta = `${hello} ${pregunta}`;
+console.log(fraseCompleta);
+
+//concatenacion anterior
+let oli = "OLANDA";
+let todos = "a todos";
+let fraseJunta = oli + ' ' + todos;
+console.log(fraseJunta);
