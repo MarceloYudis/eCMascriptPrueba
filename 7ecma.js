6685eaaac73f57dfb7b315ce54e2ca0b5893c4e0
MarceloYudis <marceloyudisortiz@gmail.com>
2022-08-29 20:25:57 -0400

    implementaciones en ecma8 y ecma7

---
+++ 7ecma.js
@@ -0,0 +1,18 @@
+//metodo include
+// comprueba si en un array se encuentra "x" valor o dato
+let numeritos = [2, 3, 4, 3, 9];
+
+if ( numeritos.includes(9)) {
+    console.log("si hay 9")
+} else {
+    console.log("no hay 9");
+};
+
+///////////////////////////////////
+//Operador de exponente en ecma7
+let numeroBase = 9;
+let exponente = 3;
+
+let resultado= numeroBase ** exponente;
+console.log(resultado);
+
