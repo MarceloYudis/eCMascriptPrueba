974729b2de73c04b5c00b668fcfc61f703ee994e
MarceloYudis <marceloyudisortiz@gmail.com>
2022-08-31 19:23:21 -0400

    uso del metdod flat y flatmap

---
+++ 10ecma.js
@@ -0,0 +1,12 @@
+let arraysito = [1,2 ,3 , [1, 2, 3, [1, 2, 3]]];
+
+console.log(array.flat(1));
+//valor de la profundida sobre la cual trabajar el metdo falt
+
+
+
+//metodo flat map
+let numeritos = [2, 33, 4, 54, 11, 99];
+console.log( numeritos.flatMap( value = > [ value, value *2]));
+
+letq
