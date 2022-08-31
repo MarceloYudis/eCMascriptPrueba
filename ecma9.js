4e32eb91cbc497e21b4a39fa84f122b811a689b1
MarceloYudis <marceloyudisortiz@gmail.com>
2022-08-31 19:19:02 -0400

    agrupar elementos en regex

--- ecma9.js
+++ ecma9.js
@@ -33,4 +33,14 @@ saludos()
     .catch(error => console.log(error))
     .finally(() => console.log('Finalizo'));
 
+ 
 
+    //como agrupar grupos de regex
+    const regexData = /([0-9]{4}) - ([0-9] {2})-([0-9]{2})/
+
+    const match = regexData.exec('2022-08-31');
+    const year = match[1];
+    const month = match[2];
+    const day = match[3]
+
+    console.log(year, month , day);
