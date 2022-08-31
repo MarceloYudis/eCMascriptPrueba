a698b2e135c7c7e2524f38b7406a202560df433e
MarceloYudis <marceloyudisortiz@gmail.com>
2022-08-30 23:18:50 -0400

    unir elementos de objetos con operadores de propagacion

--- ecma9.js
+++ ecma9.js
@@ -11,3 +11,11 @@ let { name, ...all} = cualquierObjeto;
 console.log( name, all );
 
 ///////////////////////////////
+//unir elementos de objetos a un nuevo objeto usando operadores de propagacion
+
+const ndoCualquierObjeto = {
+    ...cualquierObjeto,
+    country: 'CO',
+}
+
+console.log(ndoCualquierObjeto);
