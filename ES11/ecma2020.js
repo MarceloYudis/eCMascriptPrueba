0c6ef863254e5bd1b90660640590ae545ff74def
MarceloYudis <marceloyudisortiz@gmail.com>
2022-09-04 00:16:30 -0400

    uso del metodo BigInt

--- ES11/ecma2020.js
+++ ES11/ecma2020.js
@@ -4,3 +4,13 @@ butonsito.addEventListener("click", async function() {
     const datoExterno = await import("./arvhivo.js");
     arvhivo.saludo();
 })
+
+/////////////////////////////
+//Representar valores mayores de 2 a la 53
+const numeroGigante = 9007199254740991n;
+
+const otroNumGigante = BigInt(9007199254740991);
+
+console.log(numeroGigante);
+
+///////////////////////////
